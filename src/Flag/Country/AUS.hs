dcb743c000a5b78ac759d2b1a68a4ba8fc8feb09
Xavier Shay <contact@xaviershay.com>
2026-02-22 15:46:35 +1100

    AUS WIP

--- src/Flag/Country/AUS.hs
+++ src/Flag/Country/AUS.hs
@@ -35,37 +35,67 @@ australia = mkCountryFlag
 
     design :: Sourced :> es => Eff es (FlagA (Point, Point) Drawing)
     design = do
-        -- TODO: source dimensions from flagSpec
+        -- TODO: Proper colors, and line up with Union Jack
         whiteC <- impliedReference "White" flagSpec (sRGB24 255 255 255)
         blueC <- impliedReference "Blue" flagSpec (sRGB24 0 0 255)
         redC <- impliedReference "Red" flagSpec (sRGB24 255 0 0)
         jackArrow <- unionJack2to1
 
         pure $ proc (origin, unit) -> do
+            let unitV = (origin, unit)
             -- TODO: implement actual flag design
             (tl, tr, br, bl) <- boxNatural 2 1 -< (origin, unit)
             topMid <- midpoint -< (tl, tr)
-            top14 <- midpoint -< (tl, topMid)
-            (top14Down, _) <- perpendicular -< (top14, origin)
-            top34 <- midpoint -< (topMid, tr)
-            (_, top34Down) <- perpendicular -< (top34, tr)
-            
             leftMid <- midpoint -< (tl, bl)
-            left34 <- midpoint -< (leftMid, bl)
-            (_, left34Down) <- perpendicular -< (left34, bl)
 
-            fedCenter <- intersectLL -< ((left34, left34Down), (top14, top14Down))
-            fedRadius <- rationalMult ((3 % 20) / 2) -< (origin, unit)
-            (_, fedEdge) <- translate -< ((origin, fedRadius), fedCenter)
-            (_, fedEdgeN) <- perpendicular -< (fedCenter, fedEdge)
+            top14L  <- generateLine midpoint -< (tl, topMid)
+            top34L  <- generateLine midpoint -< (topMid, tr)
+            top5_8L  <- generateLine midpoint -< (topMid, fst top34L)
+            top4_5L <- generateLine (rationalMult (3 % 4 + (1 % 10) / 2)) -< (tl, tr)
+            top31_36L <- generateLine (rationalMult (3 % 4 + (2 % 9) / 2)) -< (tl, tr)
 
-            fedStar <- fillStar7Inner (4 % 9) redC -< (fedCenter, fedEdgeN)
+            left34L <- generateLine midpoint -< (leftMid, bl)
+            left1_6L <- generateLine (rationalMult (1 % 6)) -< (tl, bl)
+            left56L <- generateLine (rationalMult (5 % 6)) -< (tl, bl)
+            left7_16L <- generateLine (rationalMult (7 % 16)) -< (tl, bl)
+            left13_24L <- generateLine (rationalMult (1 % 2 + 1 % 24)) -< (tl, bl)
+            left89_240L <- generateLine (rationalMult (1 % 2 - 1 % 15 - 1 % 16)) -< (tl, bl)
 
-            --alphaCenter
+            -- TODO: Stars are likely half-width
+            fedStar <- drawInCircleAt ((3 % 20) / 2) (fillStar7Inner (4 % 9) whiteC)
+                -< ((left34L, top14L), unitV)
+
+            alphaStar <- drawInCircleAt ((1 % 14) / 2) (fillStar7Inner (4 % 9) whiteC)
+                -< ((left56L, top34L), unitV)
+
+            betaStar <- drawInCircleAt ((1 % 14) / 2) (fillStar7Inner (4 % 9) whiteC)
+                -< ((left7_16L, top5_8L), unitV)
+
+            gammaStar <- drawInCircleAt ((1 % 14) / 2) (fillStar7Inner (4 % 9) whiteC)
+                -< ((left1_6L, top34L), unitV)
+
+            deltaStar <- drawInCircleAt ((1 % 14) / 2) (fillStar7Inner (4 % 9) whiteC)
+                -< ((left89_240L, top31_36L), unitV)
+
+            -- TODO: 5 pointed star, not 7
+            epsilonStar <- drawInCircleAt ((1 % 24) / 2) (fillStar7Inner (4 % 9) whiteC)
+                -< ((left13_24L, top4_5L), unitV)
 
             bg <- fillRectangle blueC -< (tl, tr, br, bl)
 
             -- The canton is the upper-left quarter of a 2:1 flag.
-            --jack <- jackArrow -< (tl, topMid)
+            jack <- jackArrow -< (tl, topMid)
 
-            returnA -< bg -- <> jack
+            returnA -< bg <> jack <> fedStar <> alphaStar <> betaStar <> gammaStar <> deltaStar <> epsilonStar
+
+    generateLine pointArrow = proc (a, b) -> do
+        p      <- pointArrow -< (a, b)
+        (_, q) <- perpendicular -< (p, b)
+        returnA -< (p, q)
+
+    drawInCircleAt radiusRatio fillOp = proc ((line1, line2), (orig, scaleUnit)) -> do
+        center     <- intersectLL -< (line1, line2)
+        radius     <- rationalMult radiusRatio -< (orig, scaleUnit)
+        (_, edge)  <- translate -< ((orig, radius), center)
+        (_, edgeN) <- perpendicular -< (center, edge)
+        fillOp -< (center, edgeN)
