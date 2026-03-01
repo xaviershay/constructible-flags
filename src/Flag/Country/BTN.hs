5835455d957669a6ed10f79de57b819e1bb40b83
Xavier Shay <contact@xaviershay.com>
2026-03-02 07:02:40 +1100

    Source DZA

--- src/Flag/Country/BTN.hs
+++ src/Flag/Country/BTN.hs
@@ -1,37 +1,39 @@
+{-# LANGUAGE Arrows #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE TypeOperators #-}
-{-# LANGUAGE Arrows #-}
 
 module Flag.Country.BTN
-    ( bhutan
-    ) where
-
-import Data.Colour.SRGB (sRGB24)
-import Control.Arrow (returnA)
-import Effectful
-
-import Flag.Construction.Types (Point, Drawing, FlagA)
-import Flag.Constructions
-import Data.Ratio ((%))
-import Flag.Source
-import Flag.Pantone
-import Flag.Definition (Flag, mkCountryFlag, editorNote)
-
-bhutan :: Sourced :> es => Flag es
-bhutan = editorNote "The dragon design used here is not the most common on the internet, but better matches designs used in official capacities recently (e.g. the Olympics)."
-  $ mkCountryFlag
-  "BTN"
-  "Bhutan"
-  constructedAt
-  ( reference "Description" flagRules
-      ( "The yellow half holds the base from the top. The orange half extends from bottom and forms the fluttering end. "
-      ++ "The dragon equally spreads over the borderline. The colour of dragon is white. Whatever be the length of a "
-      ++ "larger or small national flag, two third of it must be kept as the width."
-      )
+  ( bhutan,
   )
-  design
+where
 
+import Control.Arrow (returnA)
+import Data.Colour.SRGB (sRGB24)
+import Data.Ratio ((%))
+import Effectful
+import Flag.Construction.Types (Drawing, FlagA, Point)
+import Flag.Constructions
+import Flag.Definition (Flag, editorNote, mkCountryFlag)
+import Flag.Pantone
+import Flag.Source
+
+bhutan :: (Sourced :> es) => Flag es
+bhutan =
+  editorNote "The dragon design used here is not the most common on the internet, but better matches designs used in official capacities recently (e.g. the Olympics)." $
+    mkCountryFlag
+      "BTN"
+      "Bhutan"
+      constructedAt
+      ( reference
+          "Description"
+          flagRules
+          ( "The yellow half holds the base from the top. The orange half extends from bottom and forms the fluttering end. "
+              ++ "The dragon equally spreads over the borderline. The colour of dragon is white. Whatever be the length of a "
+              ++ "larger or small national flag, two third of it must be kept as the width."
+          )
+      )
+      design
   where
     constructedAt = "2026-02-21"
     gov = mkAgentOrg "bgd_gov" "Government of Bhutan"
@@ -39,42 +41,50 @@ bhutan = editorNote "The dragon design used here is not the most common on the i
     locEntity = mkAgentOrg "loc2012" "London Organising Committee of the Olympic Games and Paralympic Games Limited"
     tocEntity = mkAgentOrg "toc2020" "Tokyo Organising Committee of the Olympic and Paralympic Games"
 
-    toc = screenshot constructedAt "btn/toc.png" $ attributeTo tocEntity $ mkEntity
-      "Flagns and Anthems Manual, Tokyo 2020"
-      "https://library.olympics.com/Default/doc/SYRACUSE/1568069/flags-and-anthems-manual-tokyo-2020-the-tokyo-organising-committee-of-the-olympic-and-paralympic-gam?_lg=en-GB"
+    toc =
+      screenshot constructedAt "btn/toc.png" $
+        attributeTo tocEntity $
+          mkEntity
+            "Flags and Anthems Manual, Tokyo 2020"
+            "https://library.olympics.com/Default/doc/SYRACUSE/1568069/flags-and-anthems-manual-tokyo-2020-the-tokyo-organising-committee-of-the-olympic-and-paralympic-gam?_lg=en-GB"
 
-    loc = screenshot constructedAt "btn/loc.png" $ attributeTo locEntity $ mkEntity
-      "Flags and Anthems Manual, London 2012"
-      "https://library.olympics.com/Default/doc/SYRACUSE/34593/flags-and-anthems-manual-london-2012-spp-final-version-london-organising-committee-of-the-olympic-ga?_lg=en-GB"
+    loc =
+      screenshot constructedAt "btn/loc.png" $
+        attributeTo locEntity $
+          mkEntity
+            "Flags and Anthems Manual, London 2012"
+            "https://library.olympics.com/Default/doc/SYRACUSE/34593/flags-and-anthems-manual-london-2012-spp-final-version-london-organising-committee-of-the-olympic-ga?_lg=en-GB"
 
-    flagRules = screenshot constructedAt "btn/flag-rules.png" $ attributeTo gov $ mkEntity
-        "The National Flag Rules of Bhutan, 1972"
-        "https://oag.gov.bt/wp-content/uploads/2011/02/National-Flag-Rules-1972-English.pdf"
+    flagRules =
+      screenshot constructedAt "btn/flag-rules.png" $
+        attributeTo gov $
+          mkEntity
+            "The National Flag Rules of Bhutan, 1972"
+            "https://oag.gov.bt/wp-content/uploads/2011/02/National-Flag-Rules-1972-English.pdf"
 
     commons = mkEntity "Flag of Bhutan alternate (Wikimedia Commons)" "https://commons.wikimedia.org/wiki/File:Flag_of_Bhutan_alternate.svg"
     fotw = mkEntity "Bhutan: Specifications Discussions (Flags of the World)" "https://www.fotw.info/flags/bt'.html#var"
 
-    design :: Sourced :> es => Eff es (FlagA (Point, Point) Drawing)
+    design :: (Sourced :> es) => Eff es (FlagA (Point, Point) Drawing)
     design = do
-        (w, h) <- reference "Proportion" flagRules (3, 2)
-        orangeC <- referencePantoneAsRGB loc ("Orange", "165-C")
-        yellowC <- referencePantoneAsRGB loc ("Yellow", "116-C")
-        dragonDesign <- reference "Dragon Design" commons "data/images/btn/dragon.svg"
-        dragonScale <- editorial "Dragon" [toc, commons, fotw] (3 % 4)
+      (w, h) <- reference "Proportion" flagRules (3, 2)
+      orangeC <- referencePantoneAsRGB loc ("Orange", "165-C")
+      yellowC <- referencePantoneAsRGB loc ("Yellow", "116-C")
+      dragonDesign <- reference "Dragon Design" commons "data/images/btn/dragon.svg"
+      dragonScale <- editorial "Dragon" [toc, commons, fotw] (3 % 4)
 
-        pure $ proc origin -> do
-            (tl, tr, br, bl) <- boxNatural w h -< origin
+      pure $ proc origin -> do
+        (tl, tr, br, bl) <- boxNatural w h -< origin
 
-            leftMid  <- midpoint -< (tl, bl)
-            rightMid <- midpoint -< (tr, br)
+        leftMid <- midpoint -< (tl, bl)
+        rightMid <- midpoint -< (tr, br)
 
-            bg1 <- fillTriangle yellowC -< (tl, tr, bl)
-            bg2 <- fillTriangle orangeC -< (bl, tr, br)
+        bg1 <- fillTriangle yellowC -< (tl, tr, bl)
+        bg2 <- fillTriangle orangeC -< (bl, tr, br)
 
-            center <- intersectLL -< ((tl, br), (tr, bl))
+        center <- intersectLL -< ((tl, br), (tr, bl))
 
-            scale <- rationalMult dragonScale -< (center, tr)
-            dragon <- overlaySVG dragonDesign -< (center, scale)
+        scale <- rationalMult dragonScale -< (center, tr)
+        dragon <- overlaySVG dragonDesign -< (center, scale)
 
-
-            returnA -< bg1 <> bg2 <> dragon
+        returnA -< bg1 <> bg2 <> dragon
--- src/Flag/Country/DZA.hs
+++ src/Flag/Country/DZA.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE Arrows #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE MultilineStrings #-}
 {-# LANGUAGE TypeOperators #-}
 
 module Flag.Country.DZA
@@ -14,34 +15,64 @@ import Data.Ratio
 import Effectful
 import Flag.Construction.Types (Drawing, FlagA, Point)
 import Flag.Constructions
-import Flag.Definition (Flag, mkCountryFlag)
+import Flag.Definition (Flag, editorNote, mkCountryFlag)
 import Flag.Pantone
 import Flag.Source
 
 algeria :: (Sourced :> es) => Flag es
 algeria =
-  mkCountryFlag
-    "DZA"
-    "Algeria"
-    constructedAt
-    (reference "Description" flagSpec "TODO: add official flag description")
-    design
+  editorNote
+    """
+    I could not find an original scan of the relevant law online, and so rely on
+    an archived page from the Permanent Mission of Algeria to the UN, courtesy
+    of Flags of the World.
+
+    Despite pouring over Rood's work, I could not figure how to interpret
+    the color instructions provided. The Pantone colors here are used
+    across most supplementary sources. (Interestingly, the one source
+    I could reference directly – the London Olympic manual – has placed the star
+    in the wrong place!)
+    """
+    $ mkCountryFlag
+      "DZA"
+      "Algeria"
+      constructedAt
+      ( reference
+          "Description"
+          flagLaw
+          """
+          The flag of the Democratic and Popular Republic of Algeria is constituted by a green and white rectangle embossed by a red star and a red crescent.
+          """
+      )
+      design
   where
     constructedAt = "2026-02-28"
     gov = mkAgentOrg "dza_gov" "Government of Algeria"
 
-    flagSpec =
-      attributeTo gov $
-        mkEntity
-          "TODO: add official flag specification title"
-          "TODO: add URL"
+    -- 1963
+    flagLaw =
+      screenshot constructedAt "dza/law.png" $
+        attributeTo gov $
+          mkEntity
+            "Appendix of the law #63-145"
+            "https://web.archive.org/web/20120205045854/http://www.algeria-un.org/default.asp?doc=-flag"
+
+    locEntity = mkAgentOrg "loc2012" "London Organising Committee of the Olympic Games and Paralympic Games Limited"
+
+    -- 2012
+    loc =
+      screenshot constructedAt "dza/loc.png" $
+        attributeTo locEntity $
+          mkEntity
+            "Flags and Anthems Manual, London 2012"
+            "https://library.olympics.com/Default/doc/SYRACUSE/34593/flags-and-anthems-manual-london-2012-spp-final-version-london-organising-committee-of-the-olympic-ga?_lg=en-GB"
 
     design :: (Sourced :> es) => Eff es (FlagA (Point, Point) Drawing)
     design = do
       -- TODO: source dimensions from flagSpec
       whiteC <- editorial "White" [] (sRGB24 255 255 255)
-      greenC <- referencePantoneAsRGB flagSpec ("Green", "356-C")
-      redC <- referencePantoneAsRGB flagSpec ("Red", "186-C")
+      greenC <- referencePantoneAsRGB loc ("Green", "356-C")
+      redC <- referencePantoneAsRGB loc ("Red", "186-C")
 
       pure $ proc (o, unit) -> do
         (tl, tr, br, bl) <- boxNatural 3 2 -< (o, unit)
